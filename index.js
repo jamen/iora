09266a7438b0bde49eb384d5a84d2b3844fbadcb
JamenMarz <jamen.marz@gmail.com>
2015-08-09 19:53:06 -0600

    Updated to follow .jshintrc linter

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 
 (function(argv, lib, path){
-  // Rewrite info for dev logging
-  global.console.old_info = global.console.info;
+  'use strict';
+
+  global.console.oldInfo = global.console.info;
   global.console.info = function(){
-    if (path.basename(process.argv[1]) === "ioradev") {
-      process.stdout.write("[ioradev] ");
-      global.console.old_info.apply(this, arguments);
+    if (path.basename(process.argv[1]) === 'ioradev') {
+      process.stdout.write('[ioradev] ');
+      global.console.oldInfo.apply(this, arguments);
     }
-  }
+  };
 
   if (process.stdin.isTTY) {
-    if (argv[0] === "run") {
+    if (argv[0] === 'run') {
       lib.core(argv, lib);
       return;
     }
-  };
+  }
 })(process.argv.slice(2),
-   require("./lib"),
-   require("path"));
+   require('./lib'),
+   require('path'));
