a2d3a5ec7ff563a80c092a57df156b73f8ff9b4e
JamenMarz <jamen.marz@gmail.com>
2015-08-09 19:55:05 -0600

    Removed dev logging

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 #!/usr/bin/env node
 
-(function(argv, lib, path){
+(function(argv, lib){
   'use strict';
-
-  global.console.oldInfo = global.console.info;
-  global.console.info = function(){
-    if (path.basename(process.argv[1]) === 'ioradev') {
-      process.stdout.write('[ioradev] ');
-      global.console.oldInfo.apply(this, arguments);
-    }
-  };
 
   if (process.stdin.isTTY) {
     if (argv[0] === 'run') {
