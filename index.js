06b9518f035069bcb6ef706907ac5db973bef562
JamenMarz <jamen.marz@gmail.com>
2015-09-13 02:26:41 -0600

    Added config loading and a test on argument routing.

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
 var lib = require('./lib'),
+    refig = require('refig'),
+    path = require('path'),
     argv = process.argv.slice(2),
     console = lib.utils;
 
+global.iora = {};
+
+/*
+ * Load configuration
+ */
+var configPath = path.join(process.cwd(), 'iora.json');
+try {
+  iora.config = refig
+    .set('async', false)
+    .read(configPath)
+    .set('async', true);
+} catch (e) {
+  console.error('Failed to read ' + configPath);
+}
 
 /*
  * Check flags
@@ -12,4 +28,13 @@ var silent =  argv.indexOf('--silent');
 if (silent !== -1) {
   argv.splice(silent, 1);
   console.silence();
+}
+
+/*
+ * Argument routing:
+ */
+if (argv[0] === 'run') {
+  // ...
+  console.log('Working!');
+  process.exit();
 }
